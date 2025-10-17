b59ab940eecb45f2754eed7d9e47165ca0ae8f14
葛未央 <geweiyang@sdpjw.com>
2025-10-17 16:44:22 +0800

    init

---
+++ src/main/java/com/nexon/nutriai/NutriaiApplication.java
@@ -0,0 +1,11 @@
+package com.nexon.nutriai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriaiApplication {
+    static void main(String[] args) {
+        SpringApplication.run(NutriaiApplication.class, args);
+    }
+}
